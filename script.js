b858d46a3600a3f4f0ecde0b7f7a6e4d5f1f61b5
emilia tongson <eftongson@gmail.com>
2019-02-07 16:31:21 -0800

    fixed boxes, removed midsection

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-alert("This is still a work in progress. Thank you for your patience while I finish coding everything! -Emilia");
+// alert("This is still a work in progress. Thank you for your patience while I finish coding everything! -Emilia");
+
+'use strict';
+console.log('wass good yall');
