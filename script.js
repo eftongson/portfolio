7ab79bd57d75a4d87f5b13a92cedc7e4158b57a0
emilia tongson <eftongson@gmail.com>
2019-05-03 12:16:06 -0700

    dcf stuff

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-alert("This is a work in progress. Thank you for your patience while I finish coding everything! -Emilia");
+alert("This is a work in progress. Thank you for your patience while I finish building this for you! -Emilia");
 
 'use strict';
 console.log('wass good yall');
