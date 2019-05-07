d78886101118b12cc9a451fb1928aca949fa48ac
emilia tongson <eftongson@gmail.com>
2019-05-07 13:56:58 -0700

    museum updae

--- inztangrat/script0.js
+++ inztangrat/script0.js
@@ -4,104 +4,12 @@ alert("This interface should only be viewed on mobile.");
 
 console.log('reading js. feed me');
 
-/* Captures the video camera and adds a filter
-   Run local sever to see in Firefox
-   */
-//
-// var capture1;
-// var capture2;
-// var imageXY;
-// var yousernameHeight;
-// // let fps = frameRate();
-//
-// // use responsive for dimensions
-// var intViewportHeight = window.innerHeight;
-// console.log('intViewportHeight: ' + intViewportHeight);
-//
-// // listens for different responsive dimensions
-// window.addEventListener('resize', function() {
-//   intViewportHeight = window.innerHeight;
-//   console.log('intViewportHeight: ' + intViewportHeight);
-// })
-//
-//
-// function setup() {
-//   imageXY = 375;
-//   yousernameHeight = 32;
-//   var feed = createCanvas(imageXY, intViewportHeight * 8);
-//   // create video capture
-//   capture1 = createCapture(VIDEO);
-//   // video capture size
-//   capture1.size(imageXY, imageXY);
-//   capture1.hide(); // remove comment to hide video
-//   // parent it to section id in html
-//   feed.parent('feed');
-//   frameRate(15);
-//   background(0, 0, 0, 0);
-//   // frameRate(15);
-//
-//   // var myPost2 = createCanvas(imageXY, imageXY);
-//
-//   // capture2 = createCapture(VIDEO);
-//   //
-//   // capture2.size(imageXY, imageXY);
-//   // capture2.hide();
-//   // myPost2.parent('myPost2');
-//
-// }
-//
-// function draw() {
-//
-//   image(capture1, 0, 4800 + yousernameHeight, 640, 480);
-//   filter('DILATE');
-//
-//   image(capture1, 0, 4000 + yousernameHeight, 640, 480);
-//   filter('DILATE');
-//
-//   image(capture1, 0, 3200 + yousernameHeight, 640, 480);
-//   filter('DILATE');
-//
-//   image(capture1, 0, 2400 + yousernameHeight, 640, 480);
-//   filter('DILATE');
-//
-//   image(capture1, 0, 1600 + yousernameHeight, 640, 480);
-//   filter('DILATE'); // change filters: THRESHOLD, GRAY, OPAQUE, INVERT, POSTERIZE, BLUR, ERODE, DILATE or BLUR.
-//
-//   image(capture1, 0, 800 + yousernameHeight, 640, 480);
-//   filter('DILATE');
-//
-//   image(capture1, 0, 0 + yousernameHeight, 640, 480);
-//   filter('ERODE');
-//
-//
-// }
-
-
-// THE FOLLOWING CODE VIA P5.JS HHELP
-// https://p5js.org/reference/#/p5.MediaElement/stop
-
-//This example both starts
-//and stops a sound sample
-//when the user clicks the canvas
-
-//We will store the p5.MediaElement
-//object in here
-// var ele;
 var feed;
 
 var capture1;
 var capture2;
 var imageDimensions;
 var yousernameHeight;
-
-// use of responsive variables for dimensions?
-// var intViewportHeight = window.innerHeight;
-// console.log('intViewportHeight: ' + intViewportHeight);
-// // listens for different responsive dimensions
-// window.addEventListener('resize', function() {
-//   intViewportHeight = window.innerHeight;
-//   console.log('intViewportHeight: ' + intViewportHeight);
-// })
 
 //while our audio is playing,
 //this will be set to true
@@ -111,6 +19,8 @@ var canvasIsDrawing = false;
 function setup() {
   imageDimensions = 375;
   yousernameHeight = 36;
+
+  frameRate(2);
 
     feed = createCanvas(imageDimensions, window.innerHeight * 8);
     background(0,0,0,0);
@@ -124,7 +34,6 @@ function setup() {
     capture1.hide(); // remove comment to hide video
     // parent it to section id in html
     feed.parent('feed');
-    frameRate(15);
     background(0, 0, 0, 0);
     // frameRate(15);
 
@@ -137,7 +46,7 @@ function setup() {
     // myPost2.parent('myPost2');
 
 }
-
+ /*
 function mouseMoved() {
   //here we test if the mouse is over the
   //canvas element when it's clicked
@@ -170,9 +79,12 @@ function mouseMoved() {
     }
   }
 }
+*/
+
 
 function draw() {
 
+  /*
   image(capture1, 0, 4800 + yousernameHeight, 640, 480);
   filter('DILATE');
 
@@ -190,6 +102,7 @@ function draw() {
 
   image(capture1, 0, 800 + yousernameHeight, 640, 480);
   filter('DILATE');
+  */
 
   image(capture1, 0, 0 + yousernameHeight, 640, 480);
   filter('ERODE');
