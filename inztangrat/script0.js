31993141472ae5ebdc2635bb8ccf26da59036215
emilia tongson <eftongson@gmail.com>
2019-05-07 14:23:51 -0700

    delete stuff to make it faster

--- inztangrat/script0.js
+++ inztangrat/script0.js
@@ -35,56 +35,14 @@ function setup() {
     // parent it to section id in html
     feed.parent('feed');
     background(0, 0, 0, 0);
-    // frameRate(15);
-
-    // var myPost2 = createCanvas(imageXY, imageXY);
-
-    // capture2 = createCapture(VIDEO);
-    //
-    // capture2.size(imageXY, imageXY);
-    // capture2.hide();
-    // myPost2.parent('myPost2');
 
 }
- /*
-function mouseMoved() {
-  //here we test if the mouse is over the
-  //canvas element when it's clicked
-  if (
-  // if the canvas is offscreen, then stop the media
-  imageDimensions>window.innerHeight
-)
-{
-    background(200);
 
-    if (canvasIsDrawing) {
-      //if the sample is currently playing
-      //calling the stop() function on
-      //our p5.MediaElement will stop
-      //it and reset its current
-      //time to 0 (i.e. it will start
-      //at the beginning the next time
-      //you play it)
-      feed.stop();
-
-      canvasIsDrawing = false;
-      // text('Click to play!', width / 2, height / 2);
-    } else {
-      //loop our sound element until we
-      //call ele.stop() on it.
-      feed.redraw();
-
-      canvasIsDrawing = true;
-      // text('Click to stop!', width / 2, height / 2);
-    }
-  }
-}
-*/
 
 
 function draw() {
 
-  
+
   image(capture1, 0, 4800 + yousernameHeight, 640, 480);
   filter('DILATE');
 
